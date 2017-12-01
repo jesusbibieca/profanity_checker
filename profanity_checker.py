0831a6a97fbd01ad82b08f4f5bbf426591eb1d47
jesusbibieca <jesusbibieca52@gmail.com>
2017-12-01 10:53:43 -0400

    improve output to users

--- profanity_checker.py
+++ profanity_checker.py
@@ -4,7 +4,7 @@ def read_text():
 	path = 'C:\Users\User\Desktop\Jesus Bibieca\profanity_checker\movie quotes.txt'
 	quotes = open(path)
 	content_of_file = quotes.read()
-	print content_of_file
+	# print content_of_file
 	quotes.close()
 	check_profanity(content_of_file)
 
@@ -12,7 +12,10 @@ def check_profanity(text_to_check):
 	url = 'http://www.wdylike.appspot.com/?q=' 
 	connection = urllib.urlopen(url + text_to_check)
 	output = connection.read()
-	print output
+	# print output
+	if "true" in output: print "Profanity Alert!"
+	elif "false" in output: print "The document has no curse words."
+	else: print "Could not scan the document properly" 
 	connection.close()
 
 read_text()
