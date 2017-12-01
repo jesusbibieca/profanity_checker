235610d7388f7c3442145cfc704fe37a310a4d3d
jesusbibieca <jesusbibieca52@gmail.com>
2017-12-01 10:45:56 -0400

    add check profanity function

--- profanity_checker.py
+++ profanity_checker.py
@@ -1,9 +1,18 @@
+import urllib
+
 def read_text():
 	path = 'C:\Users\User\Desktop\Jesus Bibieca\profanity_checker\movie quotes.txt'
 	quotes = open(path)
 	content_of_file = quotes.read()
-	return content_of_file
+	print content_of_file
 	quotes.close()
+	check_profanity(content_of_file)
 
+def check_profanity(text_to_check):
+	url = 'http://www.wdylike.appspot.com/?q=' 
+	connection = urllib.urlopen(url + text_to_check)
+	output = connection.read()
+	print output
+	connection.close()
 
-print read_text()
+read_text()
